ff8a6533b59c83b08cae9d0a4442e456f7936212
Yonathan <yonathanfridkin@gmail.com>
2024-02-29 21:48:30 +0200

    gdt

--- kernel.cpp
+++ kernel.cpp
@@ -10,6 +10,7 @@ void printf(char* str)
 typedef void (*constructor)();
 extern "C" constructor start_ctors;
 extern "C" constructor end_ctors;
+
 extern "C" void callConstructors()
 {
     for(constructor* i = &start_ctors; i != &end_ctors; i++)
