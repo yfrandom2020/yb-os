abbd34bba71e173f63f39c76f1d166b9c54bb974
Yonathan <yonathanfridkin@gmail.com>
2024-03-16 23:02:54 +0200

    gdt

--- gdt.cpp
+++ gdt.cpp
@@ -20,9 +20,11 @@ dataSegmentSelector(0,64*1024*1024, 0x92)
 
 GlobalDescriptorTable::~GlobalDescriptorTable()
 {
+    // destructor
+    // doesn't actually gets used but defining for convention
 }
 
-uint16_t GlobalDescriptorTable::DataSegmentSelector()
+uint16_t GlobalDescriptorTable::DataSegmentSelector() // return address of segment
 {
     return (uint8_t*)&dataSegmentSelector - (uint8_t*)this;
 }
@@ -30,4 +32,21 @@ uint16_t GlobalDescriptorTable::DataSegmentSelector()
 uint16_t GlobalDescriptorTable::CodeSegmentSelector()
 {
     return (uint8_t*)&codeSegmentSelector - (uint8_t*)this;
+}
+
+
+GlobalDescriptorTable::SegmentSelector::SegmentSelector(uint32_t base, uint32_t limit, uint8_t flags)
+{
+    uint8_t* target = (uint8_t*) this;
+
+    if(limit <= 65536)
+    {
+        target[6] = 0x40;
+
+    }
+    else
+    {
+
+    }
+
 }
