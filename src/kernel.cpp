f3998de3a4d3d94c19d872a1491306eae3517416
Yonathan <yonathanfridkin@gmail.com>
2024-04-24 02:22:53 +0300

    pull check

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -25,6 +25,7 @@ extern "C" void callConstructors()
 extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber)
 {
     printf("Hello World!"); // this is a string literal, stored somewhere in a non modifiable segment
+    // test pull
     GlobalDescriptorTable gdt;
     while(1);
 }
