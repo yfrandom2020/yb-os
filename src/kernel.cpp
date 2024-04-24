d4d2d6dbd2bb8b9be6ad1c51d8c5394b0ef0edeb
Yonathan <yonathanfridkin@gmail.com>
2024-04-24 19:22:23 +0300

    makefile all instruction

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -7,8 +7,44 @@ void printf(char* str)
     // now we want to have a pointer to the video memory address
     // pointer to 0xb8000
     static uint16_t* VideoMemory = (uint16_t*) 0xb8000; // right side is like saying int* x where x is 0xb8000
+    static uint8_t x = 0, y = 0;
+
     for (int i = 0; str[i] != '\0'; ++i)
-        VideoMemory[i] = (VideoMemory[i]&0xFF00) | str[i];
+    {
+        switch(str[i])
+        {
+            case '\n':
+                y++;
+                x = 0;
+                break;
+            default:
+                VideoMemory[i] = (VideoMemory[i]&0xFF00) | str[i];
+                x++;
+                break;
+
+        }
+
+        if (x >= 80)
+        {
+            y++; // next line
+            x = 0;
+        }
+
+        if (y >= 25)
+        {
+
+            // no more place on screen
+            for (y = 0; y < 25; y++)
+                for (x = 0; x < 80; x++)
+                    VideoMemory[i] = (VideoMemory[i]&0xFF00) | ' ';
+
+
+            x = 0;
+            y = 0;
+        }
+
+
+    }
 
 }
 
@@ -24,8 +60,10 @@ extern "C" void callConstructors()
 
 extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber)
 {
-    printf("Hello World!"); // this is a string literal, stored somewhere in a non modifiable segment
+    printf("Hello World! \n"); // this is a string literal, stored somewhere in a non modifiable segment
+
     // test pull
+    printf("Hello World!");
     GlobalDescriptorTable gdt;
     while(1);
 }
