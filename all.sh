65768cdba708f5941745d4a6c1e557d84220db9e
Yonathan <yonathanfridkin@gmail.com>
2024-06-01 00:51:57 +0300

    move qemu - previously used shared folder to transfer iso to windows, now running qemu on linux instead

--- all.sh
+++ all.sh
@@ -1,10 +1,9 @@
 # First create and format disk to have one partition that is ext2 defined
 # In windows machine
 
-qemu-img create -f qcow2 os-disk.qcow2 512M
+/usr/bin/qemu-img create -f qcow2 os-disk.qcow2 512M
 
 # This starts a GParted virtual machine that is used to define the ext2 partition to the hard drive used by the os 
-qemu-system-i386.exe -cdrom C:/vm-shared-2/gparted-live-1.6.0-3-i686.iso -hda os-disk.qcow2 -boot d -m 512
+/usr/bin/qemu-system-i386 -cdrom gparted-live-1.6.0-3-i686.iso -hda os-disk.qcow2 -boot d -m 512
 
-# Now use this hard drive
-qemu-system-i386.exe -cdrom C:\\vm-shared-2\\mykernel.iso -hda mydisk.qcow2 -boot d -m 512 -no-reboot -no-shutdown -d int -M smm=off -s
+bash run.sh
--- run.sh
+++ run.sh
@@ -1,3 +1,2 @@
 # on windows machine
-qemu-img create -f qcow2 mydisk.qcow2 512M
-qemu-system-i386.exe -cdrom C:\\vm-shared-2\\mykernel.iso -hda mydisk.qcow2 -boot d -m 512 -no-reboot -no-shutdown -d int -M smm=off -s
+/usr/bin/qemu-system-i386 -cdrom objects/mykernel.iso -hda os-disk.qcow2 -boot d -m 512 -no-reboot -no-shutdown -d int -M smm=off -s
